cf76883c224c1321ca5e4d615d0de4fe7caedf85
Pierre Ghueldre <ghueldre.pierre@laposte.net>
2024-05-31 09:45:30 +0200

    ajout test date

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -31,7 +31,6 @@ const EventList = () => {
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
-    console.info("evtType", evtType);
   };
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
